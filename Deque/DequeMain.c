c385bd8c07cccb86d49275be0960c19abe01b0c4
최병준 <qudwns2052@gmail.com>
2019-02-07 17:34:53 +0900

    190207

---
+++ Deque/DequeMain.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "Deque.h"
+
+int main(void)
+{
+	Deque deq;
+	DequeInit(&deq);
+
+	DQAddFirst(&deq, 3);
+	DQAddFirst(&deq, 2);
+	DQAddFirst(&deq, 1);
+
+	DQAddLast(&deq, 4);
+	DQAddLast(&deq, 5);
+	DQAddLast(&deq, 6);
+
+	while (!DQIsEmpty(&deq))
+		printf("%d ", DQRemoveFirst(&deq));
+
+	printf("\n\n");
+
+	DQAddFirst(&deq, 3);
+	DQAddFirst(&deq, 2);
+	DQAddFirst(&deq, 1);
+
+	DQAddLast(&deq, 4);
+	DQAddLast(&deq, 5);
+	DQAddLast(&deq, 6);
+
+	while (!DQIsEmpty(&deq))
+		printf("%d ", DQRemoveLast(&deq));
+
+	int i;
+	scanf("%d", &i);
+
+	return 0;
+
+}
