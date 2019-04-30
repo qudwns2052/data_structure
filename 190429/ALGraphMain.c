8eed9a7656c10f7c9c619a675a31409cad4b0170
최병준 <qudwns2052@gmail.com>
2019-04-30 10:10:27 +0900

    190430

---
+++ 190429/ALGraphMain.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "ALGraph.h"
+#include <windows.h>
+
+int main(void)
+{
+	ALGraph graph;
+	GraphInit(&graph, 5);
+
+	AddEdge(&graph, A, B);
+	AddEdge(&graph, A, D);
+	AddEdge(&graph, B, C);
+	AddEdge(&graph, C, D);
+	AddEdge(&graph, D, E);
+	AddEdge(&graph, E, A);
+
+	ShowGraphEdgeInfo(&graph);
+	GraphDestroy(&graph);
+
+	system("pause");
+	return 0;
+}
